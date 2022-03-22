be89348493d2319ef2d26fe61b299a0ff39f18af
sarra <chakersarara@gmail.com>
2022-03-22 21:49:15 +0100

    modification

--- ex1/database/connect.js
+++ ex1/database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 mongoose.connect(process.env.DATABASE_URL)
 .then(()=>{
-    console.log("Application connecté à la base de données")
+    console.log("Application connectée à la base de données")
 })
 .catch(err=>
     console.log("Erreur de connection à la base de données :",err.message)
